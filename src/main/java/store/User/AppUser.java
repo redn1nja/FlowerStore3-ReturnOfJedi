8e6537250299b96c65a570ca886f0c6cb99b6e42
redn1njaa <ostap.seryvko@ucu.edu.ua>
2022-11-14 15:12:21 +0200

    heroku.... i forgor....

--- src/main/java/store/User/AppUser.java
+++ src/main/java/store/User/AppUser.java
@@ -1,28 +1,31 @@
-//package com.User;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.Id;
-//import javax.persistence.Transient;
-//import java.time.LocalDate;
-//import java.time.Period;
-//
-//
-//@Getter @Setter
-//public class AppUser {
-//    @Id
-//    @GeneratedValue
-//    private int id;
-//    private String name;
-//    private String email;
-//    @Transient
-//    private int age;
-//    private LocalDate dob;
-//
-//    public int getAge(){
-//        return Period.between(dob, LocalDate.now()).getYears();
-//    }
-//
-//}
+package store.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+
+@Getter @Setter @Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String email;
+    @Transient
+    private int age;
+    private LocalDate dob;
+
+    public int getAge(){
+        return Period.between(dob, LocalDate.now()).getYears();
+    }
+
+}
--- src/main/java/store/User/AppUserRepository.java
+++ src/main/java/store/User/AppUserRepository.java
@@ -1,10 +1,10 @@
-//package com.User;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//@Repository
-//public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
-//    Optional<AppUser> findAppUserByEmail(String email);
-//}
+package store.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+    Optional<AppUser> findAppUserByEmail(String email);
+}
